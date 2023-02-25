06cb45ba89ec420c0adbd046b2cb8b62285e4a4d
Olintho S. Jesus <olinthojesus@yahoo.com.br>
2023-02-25 00:50:47 -0300

    First steps

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const persons = [
+    {
+      "id": 1,
+      "name": "Olintho"
+    },
+    {
+      "id": 2,
+      "name": "Roseli"
+    },
+    {
+      "id": 3,
+      "name": "Erica"
+    },
+    {
+      "id": 4,
+      "name": "Isabela"
+    }
+  ]
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <form>
+        
+        <input id="id" type="text" placeholder="id" />
+        <input id="Nome" type="text" placeholder="Nome da Pessoa"/>
+        <button>+Pessoa</button>
+      </form>
+      <div className="mt-3">
+          <span>Iniciando o projeto</span>
+            <ul className="list-group">
+              {persons.map(person => (
+                <li key={person.id} className="list-group-item">
+                  {person.id } - {person.name}
+                </li>
+              ))}
+            </ul>
+      </div>
+    </>
   );
 }
 
